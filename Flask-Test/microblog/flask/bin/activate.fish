72b647d755a108a8813db31c026d2b45523e0d32
dylan <dylan@dylan-debian>
2014-02-14 16:07:49 -0800

    Added API

--- Flask-Test/microblog/flask/bin/activate.fish
+++ Flask-Test/microblog/flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dylan/Documents/Projects/Flask-Test/Internme/flask_test/microblog/flask"
+set -gx VIRTUAL_ENV "/home/dylan/Documents/Projects/Flask-Test/microblog/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
